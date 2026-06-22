d0b835953ca92569530e568f59502dd90b5f7790
Buck Williams <sbuckwilliams@icloud.com>
2026-06-22 16:47:30 -0500

    created grid and click detection
    created a grid that is automatically filled with black squares, then a
    loop that detects where a click was made and alternates that pixel. Also
    implemented a "states" array that keeps track of all of the states of
    each pixel

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("John Conway's Game of Life")
-root.geometry("300x150")
+window = tk.Tk()
 
-label = tk.Label(root, text="Hello, World!", font=("Arial", 16))
-label.pack(pady=20)
+# Set gridsize for number of pixels in rows/columns
+# Set pixelsize for actual size of individual pixel
+gridsize = 32
+pixelsize = 15
 
-root.mainloop()
+# Set all states to dead initially
+states = [
+    [0 for _ in range(gridsize)]
+    for _ in range(gridsize)
+]
+
+# function for toggling pixel
+def togglepixel(row, col):
+    rect = pixels[row][col]
+
+    if states[row][col] == 0:
+        canvas.itemconfig(rect, fill="white")
+        states[row][col] = 1
+    elif states[row][col] == 1:
+        canvas.itemconfig(rect, fill="black")
+        states[row][col] = 0
+
+# Create canvas
+canvas = tk.Canvas(window, width = gridsize*pixelsize, height= gridsize*pixelsize)
+canvas.pack()
+
+def click(event):
+    row = event.y // pixelsize
+    col = event.x // pixelsize
+
+    togglepixel(row, col)
+
+pixels = []
+
+# fill grid
+for y in range(gridsize):
+    row = []
+    for x in range(gridsize):
+        rect = canvas.create_rectangle(
+            x*pixelsize,
+            y*pixelsize,
+            (x+1)*pixelsize,
+            (y+1)*pixelsize,
+            fill="black",
+            outline=""
+        )
+        row.append(rect)
+    pixels.append(row)
+
+canvas.bind("<Button-1>", click)
+
+window.mainloop()
