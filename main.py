773b9a99a318922bb3d33bb87187bdd17ff5e93f
Buck Williams <sbuckwilliams@icloud.com>
2026-06-23 14:55:53 -0500

    Added main update system
    Added main update system triggered on spacebar. Created several
    functions that redraw, calculate the next generation, and update the
    board.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import tkinter as tk
+import time
 
 window = tk.Tk()
 
 # Set gridsize for number of pixels in rows/columns
 # Set pixelsize for actual size of individual pixel
-gridsize = 32
-pixelsize = 15
+gridsize = 64
+pixelsize = 20
 
 # Set all states to dead initially
 states = [
+    [0 for _ in range(gridsize)]
+    for _ in range(gridsize)
+]
+# Create blank array for newstates
+newstates = [
     [0 for _ in range(gridsize)]
     for _ in range(gridsize)
 ]
@@ -51,6 +57,45 @@ for y in range(gridsize):
         row.append(rect)
     pixels.append(row)
 
+def nextgen(row, col):
+    state = states[row][col]
+    surrcount = 0;
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            surrcount += states[row - 1 + j][col - 1 + i]
+    # live rules:
+    if state == 1:
+        if surrcount < 2:
+            newstates[row][col] = 0
+        elif surrcount == 2 or surrcount == 3:
+            newstates[row][col] = 1
+        elif surrcount > 3:
+            newstates[row][col] = 0
+    elif state == 0:
+        if surrcount == 3:
+            newstates[row][col] = 1
+        else:
+            newstates[row][col] = 0
+
+def reload():
+    global states
+    states = [row[:] for row in newstates]
+    for i in range(gridsize):
+        for j in range(gridsize):
+            if states[i][j] == 1:
+                canvas.itemconfig(pixels[i][j], fill = "white")
+            elif states[i][j] == 0:
+                canvas.itemconfig(pixels[i][j], fill = "black")
+
+def updateall(event):
+    for i in range(gridsize-2):
+        for j in range(gridsize-2):
+            nextgen(i+1,j+1)
+    reload()
+
 canvas.bind("<Button-1>", click)
+window.bind("<space>", updateall)
 
 window.mainloop()
